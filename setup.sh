6c8cefb0a931db8cbbd18ea664c5ba59cb51eb32
Vikki Verma <154298522+Conscious-TechHub@users.noreply.github.com>
2024-04-03 03:22:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
-sudo chmod +x openlogic-openjdk-8u392-b08-linux-x64-deb.deb
-
-sudo dpkg-i openlogic-openjdk-8u392-b08-linux-x64-deb.deb
-
-sudo update-alternative --config java
-
-cd Burpsuite
 
 sudo chmod +x *
 
-sudo java-jar ESEdition.jar
+sudo dpkg -i openlogic-openjdk-8u392-b08-linux-x64-deb.deb
 
+sudo update-alternatives --config java
+
+sudo java -jar ESEdition.jar
