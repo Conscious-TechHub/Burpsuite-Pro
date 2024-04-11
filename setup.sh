cda22ff96b0c5c0a728973f664c85df511fcb51a
Vikki Verma <154298522+Conscious-TechHub@users.noreply.github.com>
2024-04-12 04:42:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-sudo xdg-open https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u392-b08/openlogic-openjdk-8u392-b08-linux-x32-deb.deb
-
-# sudo xdg-open https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u392-b08/openlogic-openjdk-8u392-b08-linux-x64-deb.deb
-
 sudo chmod +x *
 
 sudo dpkg -i openlogic-openjdk-8u392-b08-linux-x64-deb.deb
